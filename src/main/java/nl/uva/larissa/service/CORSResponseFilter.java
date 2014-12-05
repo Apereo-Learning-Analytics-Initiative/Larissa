23fc3167d8c054e0483486d2bbf0dfa2e56df551
ernestojanus <e.j.verhoeven@uva.nl>
2014-12-05 16:11:29 +0100

    xapi-version required in response headers

--- src/main/java/nl/uva/larissa/service/CORSResponseFilter.java
+++ src/main/java/nl/uva/larissa/service/CORSResponseFilter.java
@@ -15,10 +15,18 @@ public class CORSResponseFilter implements ContainerResponseFilter {
 		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 
 		headers.add("Access-Control-Allow-Origin", "*");
-		headers.add("Access-Control-Allow-Methods", "HEAD, POST, GET, OPTIONS, DELETE, PUT");
+		headers.add("Access-Control-Allow-Methods",
+				"HEAD, POST, GET, OPTIONS, DELETE, PUT");
 		headers.add(
 				"Access-Control-Allow-Headers",
 				"Content-Type,Content-Length,Authorization,If-Match,If-None-Match,X-Experience-API-Version, Accept-Language");
+
+		/*
+		 * 6.2 API Versioning
+		 * The LRS MUST include the "X-Experience-API-Version" header in every
+		 * response. The LRS MUST set this header to "1.0.1".
+		 */
+		headers.add("X-Experience-API-Version", "1.0.1");
 	}
 
 }
